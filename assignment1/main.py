87a2d89a7d46c16842d3ea9f45d486c9c7689549
pl2599 <laichristopher99@yahoo.com>
2020-07-06 12:57:17 -0400

    added hyperparameter of assignment1

--- assignment1/main.py
+++ assignment1/main.py
@@ -5,9 +5,16 @@ if sys.version_info[0] < 3:
     raise Exception("Python 3 not detected.")
 
 
+# Booleans to run specific code chunks
+plot_curves = False
+tune_hyperparameters = False
+cross_validate = True
+
+
 # Set Up
 import numpy as np
 import pandas as pd
+import itertools
 import matplotlib.pyplot as plt
 from sklearn import svm
 from sklearn.metrics import accuracy_score
@@ -104,13 +111,105 @@ def plot_learning_curve(X_train_all, X_val_all, y_train_all, y_val_all, train_si
     plt.savefig('figs/' + title + '_learning_curve.png')
 
 
-mnist_train_sizes = [100, 200, 500, 1000, 2000, 5000, 10000]
-spam_train_sizes = [100, 200, 500, 1000, 2000, len(spam_X_train)]
-cifar10_train_sizes = [100, 200, 500, 1000, 2000, 5000]
+if plot_curves:
 
-# Plot Learning Curves
-plot_learning_curve(mnist_X_train, mnist_X_val, mnist_y_train, mnist_y_val, mnist_train_sizes, 'mnist')
-plot_learning_curve(spam_X_train, spam_X_val, spam_y_train, spam_y_val, spam_train_sizes, 'spam')
-plot_learning_curve(cifar10_X_train, cifar10_X_val, cifar10_y_train, cifar10_y_val, cifar10_train_sizes, 'cifar10')
+    # Determine Training Sizes
+    mnist_train_sizes = [100, 200, 500, 1000, 2000, 5000, 10000]
+    spam_train_sizes = [100, 200, 500, 1000, 2000, len(spam_X_train)]
+    cifar10_train_sizes = [100, 200, 500, 1000, 2000, 5000]
 
+    # Plot Learning Curves
+    plot_learning_curve(mnist_X_train, mnist_X_val, mnist_y_train, mnist_y_val, mnist_train_sizes, 'mnist')
+    plot_learning_curve(spam_X_train, spam_X_val, spam_y_train, spam_y_val, spam_train_sizes, 'spam')
+    plot_learning_curve(cifar10_X_train, cifar10_X_val, cifar10_y_train, cifar10_y_val, cifar10_train_sizes, 'cifar10')
+
+
+# Hyperparameter Tuning
+
+def cartesian_product(dic):
+    """Helper Function to Get the Cartesian Product of all hyperparameters
+
+    Args:
+        dic (dictionary): Dictionary housing all hyperparameters
+
+    Returns:
+        List: List of dictionary combinations
+    """
+    keys = dic.keys()
+    values = dic.values()
+    return [dict(zip(keys, x)) for x in itertools.product(*values)]
+
+
+
+def hyperparameter_tune(X_train_all, y_train_all, X_val_all, y_val_all, hyperparameters, train_size=None):
+    """Function to tune hyperparameters using validation set
+
+    Args:
+        X_train (numpy array): Training Data Examples
+        y_train (numpy array): Labels for training data
+        X_val (numpy array): Validation Data Examples
+        y_val (numpy array): Labels for validation data
+        hyperparameters (Dictionary): Dictionary of Hyperparameters to be tuned
+        train_size (integer, optional): Size of the Trainign Data. Defaults to None.
+
+    Returns:
+        (float, dictionary): Returns the best score and best parameters
+    """
+    # Initialize train_size to be the full train data if no parameters passed
+    if train_size is None:
+        train_size = len(X_train_all)
+
+    # Select Subset of Data
+    X_train = X_train_all[:train_size]
+    X_val = X_val_all[:train_size]
+    y_train = y_train_all[:train_size]
+    y_val = y_val_all[:train_size]
+
+    # Create Grid of hyperparameters
+    grid = cartesian_product(hyperparameters)
+
+    # Loop through hyperparameters 
+    best_score = 0
+    for hyperparameter in grid:
+        # Initialize Model
+        model = svm.SVC(kernel='linear', **hyperparameter)
+
+        # Fit Model
+        print(f"Training using hyperparameters: {hyperparameter}")
+        model.fit(X_train, y_train)
+
+        # Predict Values on Validation Set
+        val_pred = model.predict(X_val)
+
+        # Get Accuracy
+        score = accuracy_score(y_val, val_pred)
+        print(f"Accuracy Score: {score}")
+
+        if score > best_score:
+            best_score = score
+            best_parameters = hyperparameter
+    
+    return best_score, best_parameters
+
+
+
+if tune_hyperparameters:
+    # Define Hyperparameters 
+    hyperparameters = {
+        'C': [10 ** x for x in range(-4, 4)]
+        }
+
+    # Get the best score and best parameters
+    best_score, best_parameters = hyperparameter_tune(
+        mnist_X_train, 
+        mnist_y_train, 
+        mnist_X_val, 
+        mnist_y_val, 
+        hyperparameters=hyperparameters, 
+        train_size=10000
+    )
+
+    print(f"The Best Accuracy is: {best_score}")
+    print(f"with the following parameters: {best_parameters}")
+    
 
